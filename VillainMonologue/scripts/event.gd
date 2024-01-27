46bcef4410442f15b4443ec551036257c6c5649e
Miles <meaijala@gmail.com>
2024-01-27 22:50:06 +0200

    Fixed merge issues

--- VillainMonologue/scripts/event.gd
+++ VillainMonologue/scripts/event.gd
@@ -4,6 +4,8 @@ extends Node
 var main
 var sound_library
 
+var current_malice = 20
+
 # FUNCTIONS TO CALL FROM DIALOGUE:
 
 func start_timer():
@@ -11,6 +13,16 @@ func start_timer():
 
 func stop_timer():
 	main.stop_timer()
+
+func adjust_malice(amount):
+	current_malice += amount
+	main.adjust_malice(current_malice)
+	
+	if current_malice >= 100:
+		end_game_max_evil()
+	elif current_malice <= 0:
+		end_game_max_nice()
+
 
 # Play a sound from the current scene's sound library
 func play_sound(sound : String):
@@ -22,3 +34,6 @@ func end_game_max_evil():
 	# play cutscene
 	# -> credits
 	print ("game ended with maximum evil meter")
+
+func end_game_max_nice():
+	print ("game ended with no evil. sad")
--- VillainMonologue/scripts/main.gd
+++ VillainMonologue/scripts/main.gd
@@ -1,32 +1,15 @@
 extends Node2D
 
-<<<<<<< HEAD
-# When scene loads, get the TextureProgressBar in scene
-func _ready():
-	var malice_progress_bar : TextureProgressBar = get_node("CanvasLayer/TextureProgressBar")
-
-# If A-key is pressed on the keyboard, adjusts the value of the progress bar
-func _increase_malice(malice_progress_bar):
-	if Input.is_key_pressed(KEY_A):
-		var new_malice_value = malice_progress_bar.value + 20
-		malice_progress_bar._adjust_malice_value(new_malice_value)
-	if Input.is_key_pressed(KEY_A) && malice_progress_bar.value == 100:
-		var new_malice_value = malice_progress_bar.value - 10
-		malice_progress_bar._adjust_malice_value(new_malice_value)
-
-# Calls the _increase_malice function every frame
-func _process(delta):
-	var malice_progress_bar : TextureProgressBar = get_node("CanvasLayer/TextureProgressBar")
-	_increase_malice(malice_progress_bar)
-=======
 @export var timer_time = 10.0 ## Time allowed for dialogue responses
 
 # Load some files and nodes
 @onready var dialogue_file = load("res://dialogue.dialogue")
 @onready var timer = %Timer
+@onready var malice_progress_bar : TextureProgressBar = get_node("CanvasLayer/TextureProgressBar")
 var dialogue = false
 
 # At start of game, play cutscene animations and then start dialogue
+# Also et the TextureProgressBar in scene
 func _ready():
 	timer.hide()
 	Event.main = self # Update Event on this node's location
@@ -39,6 +22,11 @@ func _ready():
 		DialogueManager.show_dialogue_balloon(dialogue_file, "start1")
 		dialogue = false
 
+# If A-key is pressed on the keyboard, adjusts the value of the progress bar
+func adjust_malice(new_malice_value):
+	malice_progress_bar._adjust_malice_value(new_malice_value)
+
+# Dialogue countdown timer functions
 func start_timer():
 	await timer.start_timer(timer_time)
 	# autoselect one of the dialogue options
@@ -46,4 +34,3 @@ func start_timer():
 
 func stop_timer():
 	timer.stop_timer()
->>>>>>> Mosus_event_and_dialogue
