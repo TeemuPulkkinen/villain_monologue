88f07a03d24556c9235f202bf733b74a7fcfa1c2
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2024-01-29 16:01:40 +0200

    Add clicking sound to the main menu button in credits menu

--- VillainMonologue/scripts/MainMenuButton.gd
+++ VillainMonologue/scripts/MainMenuButton.gd
@@ -2,4 +2,5 @@ extends TextureButton
 
 # Goes to main menu after game is over from credits scene
 func _on_button_pressed():
+	Event.play_sound("Click")
 	get_tree().change_scene_to_file("res://menus/main_menu.tscn")
